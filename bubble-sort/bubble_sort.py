df7eecf93216f947aa0a32016d7910df321a76f2
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 21:23:09 +0600

    bubble-sort task

--- bubble-sort/bubble_sort.py
+++ bubble-sort/bubble_sort.py
@@ -1,2 +1,15 @@
 def bubble_sort(lst):
     # sort the list via bubble sort
+    while not isSorted(lst):
+        for i in range(len(lst)):
+            if i != len(lst)-1:
+                if lst[i] > lst[i + 1]:
+                    lst[i], lst[i+1] = lst[i+1], lst[i]
+    return lst
+
+def isSorted(lst):
+    for i in range(len(lst)):
+        if i != len(lst)-1:
+            if lst[i] > lst[i + 1]:
+                return False
+    return True
