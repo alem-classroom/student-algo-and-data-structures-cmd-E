0f4bccf63a473eebda1b91e8bb1d5f8245ee7061
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 20:55:54 +0600

    sorting task

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,2 +1,12 @@
 def selection_sort(lst):
     # sort the list via selection sort
+    if len(lst) == 1:
+        return lst
+    
+    for i in range(len(lst)):
+        theSmallest = i
+        for j in range(i+1, len(lst)):
+            if lst[j] < lst[theSmallest]:
+                theSmallest = j
+        lst[i], lst[theSmallest] = lst[theSmallest], lst[i]
+    return lst
