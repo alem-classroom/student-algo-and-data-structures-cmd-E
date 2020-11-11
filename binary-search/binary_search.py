7346d2e36f77de554fe018d3b66b204c7e51fbcf
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 11:48:10 +0600

    binary-search task

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -2,7 +2,6 @@ def binary_search(lst, to_find):
     # search for the element to_find inside lst
     # if found, return index of element
     # else return -1
-    # Reccursion variant
     middleIndex = int(len(lst) / 2)
     suspect = lst[middleIndex]
     if to_find not in lst:
@@ -13,6 +12,3 @@ def binary_search(lst, to_find):
         return binary_search(lst[:middleIndex], to_find)
     elif suspect < to_find:
         return binary_search(lst[middleIndex:], to_find)
-    else:
-        return -1
-# print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
