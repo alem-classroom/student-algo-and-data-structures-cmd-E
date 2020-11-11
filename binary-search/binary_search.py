e12a574b292870191a899f0309c5d93fa183dc5a
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 11:45:43 +0600

    binary-search task

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,10 +1,12 @@
-import math
 def binary_search(lst, to_find):
     # search for the element to_find inside lst
     # if found, return index of element
     # else return -1
-    middleIndex = math.floor(len(lst)/2)
+    # Reccursion variant
+    middleIndex = int(len(lst) / 2)
     suspect = lst[middleIndex]
+    if to_find not in lst:
+        return -1
     if suspect == to_find:
         return lst[middleIndex]
     elif suspect > to_find:
