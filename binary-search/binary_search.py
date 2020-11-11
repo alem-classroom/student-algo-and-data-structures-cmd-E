4c4e562169c303a851b7ea499cfcf224cbd0b2fa
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 11:39:49 +0600

    binary-search task

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,4 +1,16 @@
+import math
 def binary_search(lst, to_find):
     # search for the element to_find inside lst
     # if found, return index of element
     # else return -1
+    middleIndex = math.floor(len(lst)/2)
+    suspect = lst[middleIndex]
+    if suspect == to_find:
+        return lst[middleIndex]
+    elif suspect > to_find:
+        return binary_search(lst[:middleIndex], to_find)
+    elif suspect < to_find:
+        return binary_search(lst[middleIndex:], to_find)
+    else:
+        return -1
+# print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
