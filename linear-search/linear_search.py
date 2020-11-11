c3fa3d7fd5d09d8e148fdd332520e80145afcf7d
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 10:57:53 +0600

    linear-search task

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -2,3 +2,7 @@ def linear_search(lst, to_find):
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
+  for index in range(len(lst)):
+    if lst[index] == to_find:
+      return index
+  return -1
