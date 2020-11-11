ce63457a8f1bf4d512f2ee22a9217fcfcaa27764
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 11:06:36 +0600

    linear-search task

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -4,6 +4,7 @@ def linear_search(lst, to_find):
   # else return -1
   for index in range(len(lst)):
     if lst[index] == to_find:
-      return index + 1
+      return lst[index]
   return -1
+
 print(linear_search([1, 3, 4, 5, 23, 12], 1))
